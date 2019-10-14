5d8882417a4030d2ee3eff00be34eec696e645e9
Ivan Dilchovski <ivan.dilchovski@insitex.com>
2019-10-14 19:23:58 +0300

    Add rudimentary NetworkError Decode models via Codable

--- Sources/CityNetworking/Network.swift
+++ Sources/CityNetworking/Network.swift
@@ -9,7 +9,12 @@ import Foundation
 
 protocol Network {
     func request<T: Codable>(endpoint: Endpoint,
-                 handler: ((Result<T, Error>) -> ())?) -> NetworkRequest
+                 handler: @escaping (Result<T, NetworkError>) -> ()) -> NetworkRequest
+}
+
+enum NetworkError: Error {
+    case statusCode(Int)
+    case other(Error)
 }
 
 class DefaultNetwork: Network {
@@ -20,15 +25,25 @@ class DefaultNetwork: Network {
     }
     
     func request<T: Codable>(endpoint: Endpoint,
-                             handler: ((Result<T, Error>) -> ())?) -> NetworkRequest {
+                             handler: @escaping (Result<T, NetworkError>) -> ()) -> NetworkRequest {
 
-        return provider.request(endpoint: endpoint, responseHandler: { result in
+        return provider.request(endpoint: endpoint, dataHandler: { result in
             switch result {
             case .failure(let error):
-                handler?(Result.failure(error))
+                handler(Result.failure(.other(error)))
 
-            case .success(let urlResponse):
-                break
+            case .success(let (response, data)):
+                guard response.statusCode >= 200 && response.statusCode < 300 else {
+                    handler(Result.failure(.statusCode(response.statusCode)))
+                    return
+                }
+                
+                do {
+                    let model = try JSONDecoder().decode(T.self, from: data)
+                    handler(Result.success(model))
+                } catch {
+                    handler(Result.failure(.other(error)))
+                }
             }
         })
     }
